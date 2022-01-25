1a931049116365d13c0405509a8089e52ada5a24
amir maleki <amirmaleki21physics@gmail.com>
2022-01-25 14:51:04 +0330

    add text area for additional descript

--- src/components/TranseActionForm.js
+++ src/components/TranseActionForm.js
@@ -8,7 +8,10 @@ const TranseActionForm = ({ addToTransActionsList }) => {
     descript: "",
     amount: "",
     type: "",
+    additionDescript : "",
   });
+  const [isShowDescriptInp, setIsShowDescriptInp] = useState(false);
+
   const changeHandler = (event) => {
     setFormValues({ ...formValues, [event.target.name]: event.target.value });
   };
@@ -32,7 +35,7 @@ const TranseActionForm = ({ addToTransActionsList }) => {
           name="descript"
           onChange={changeHandler}
           value={formValues.descript}
-          className="rounded-xl px-3 py-2 outline-none my-2 w-full"
+          className="rounded-xl px-3 py-2 outline-none my-2 w-full shadow"
         />
         <input
           type="number"
@@ -41,12 +44,39 @@ const TranseActionForm = ({ addToTransActionsList }) => {
           name="amount"
           onChange={changeHandler}
           value={formValues.amount}
-          className="rounded-xl px-3 py-2 outline-none my-2 w-full"
+          className="rounded-xl px-3 py-2 outline-none my-2 w-full shadow"
         />
+
+        <div className="addition-descrpt mb-4">
+          <input
+            type="checkbox"
+            id="addDescrptInput"
+            className="mx-1"
+            onClick={() => setIsShowDescriptInp((prevState) => !prevState)}
+          />
+          <label
+            htmlFor="addDescrptInput"
+            className="text-gray-400 text-xs sm:text-base mx-auto"
+          >
+            add desciption for transeAction
+          </label>
+          <div>
+            {isShowDescriptInp && (
+              <textarea
+                type="text"
+                name="additionDescript"
+                onChange={changeHandler}
+                className="rounded-xl px-3 py-2 outline-none my-2 w-full shadow"
+                placeholder="Additional explanation"
+              />
+            )}
+          </div>
+        </div>
+
         <div className="flex items-center justify-around">
           <div className="mx-3 flex items-center">
             <input
-            id="incomeInput"
+              id="incomeInput"
               type="radio"
               value="income"
               name="type"
@@ -59,24 +89,27 @@ const TranseActionForm = ({ addToTransActionsList }) => {
           </div>
           <div className="mx-3 flex items-center">
             <input
-            id="expenseInput"
+              id="expenseInput"
               type="radio"
               value="expense"
               name="type"
               onChange={changeHandler}
               checked={formValues.type === "expense"}
             />
-            <label htmlFor="expenseInput" className="mx-1 cursor-pointer">expense</label>
+            <label htmlFor="expenseInput" className="mx-1 cursor-pointer">
+              expense
+            </label>
           </div>
         </div>
+
         <button
-          className="border border-purple-800 px-2 text-purple-800 rounded mt-3 w-5/6 mx-auto"
+          className="border border-purple-800 px-2 bg-purple-800 text-white rounded mt-3 w-5/6 mx-auto hover:bg-gray hover:text-purple-800"
           type="submit"
           onClick={addtranseActionHandler}
         >
           Add
         </button>
-        <ToastContainer  position={"top-center"}/>
+        <ToastContainer position={"top-center"} />
       </form>
     </>
   );
